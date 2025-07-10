6ccc3dc06d624c74a893c56d1bcc9bcb8d2850d9
Neo Adrian <134072533+MaiTheHao@users.noreply.github.com>
2025-07-10 15:21:25 +0700

    chore(home page): final

--- src/components/pages/home/work-process/StepContent.jsx
+++ src/components/pages/home/work-process/StepContent.jsx
@@ -4,7 +4,7 @@ import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
 
 function StepContent({ currentStep }) {
 	return (
-		<div className='flex flex-col-reverse md:grid md:grid-cols-2 gap-0 items-stretch md:border-2 border-[var(--color-bg-accent)] md:rounded-2xl overflow-hidden'>
+		<div className='flex flex-col-reverse md:grid md:grid-cols-2 gap-0 items-stretch md:border-2 border-[var(--color-bg-accent)] md:rounded-lg overflow-hidden'>
 			<div className='flex flex-col gap-6 md:gap-8 md:px-4 md:px-6 lg:px-8 py-6 md:py-6 lg:py-8'>
 				<div className='flex flex-col gap-3 md:gap-4'>
 					<h3 className='text-2xl md:text-3xl xl:text-4xl font-bold text-[var(--color-text-header)] font-title leading-tight'>
