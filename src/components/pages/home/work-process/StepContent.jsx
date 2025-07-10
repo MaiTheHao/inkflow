a056ade3ec02223c33200a84f7c97219c76f2078
Neo Adrian <134072533+MaiTheHao@users.noreply.github.com>
2025-07-10 18:13:05 +0700

    chore

--- src/components/pages/home/work-process/StepContent.jsx
+++ src/components/pages/home/work-process/StepContent.jsx
@@ -10,7 +10,7 @@ function StepContent({ currentStep }) {
 					<h3 className='text-2xl md:text-3xl xl:text-4xl font-bold text-[var(--color-text-header)] font-title leading-tight'>
 						{currentStep.title}
 					</h3>
-					<p className='text-[var(--color-text-paragraph)] leading-relaxed text-base'>
+					<p className='text-[var(--color-text-paragraph)] leading-relaxed text-sm md:text-base'>
 						{currentStep.description}
 					</p>
 				</div>
